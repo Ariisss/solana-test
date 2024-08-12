7f0b84545879ac22d558fab59fbe6aabb8960dd0
Ariisss <arispantino@gmail.com>
2024-08-12 21:06:22 +0800

    feat: implemented rust program

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+use solana_program::{
+    account_info:: AccountInfo,
+    entrypoint,
+    entrypoint:: ProgramResult,
+    pubkey:: Pubkey,
+    msg,
+};
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+
+    msg!("Hello, World!");
+
+    Ok(())
+}
+
+// pub fn add(left: u64, right: u64) -> u64 {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
